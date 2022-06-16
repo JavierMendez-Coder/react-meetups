e826742e24ef2e76f007e6ee4f9d34c8dd57c215
Javier Méndez <javiermendez0299@gmail.com>
2022-06-16 09:04:30 -0700

    Add "head" metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,7 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
+
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
@@ -17,10 +20,21 @@ function NewMeetupPage() {
 
     console.log(data);
 
-    router.push("/")
+    router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
